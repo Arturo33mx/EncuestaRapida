c512f8eeeccb130615242bb8b0594a222db6ad5a
Arturo Corona <arturo44full@gmail.com>
2018-03-16 18:38:07 -0600

    d

--- CapturaDis22/Reportes/ExcelGeneral.php
+++ CapturaDis22/Reportes/ExcelGeneral.php
@@ -28,7 +28,14 @@ $styleArray = array(
 		),
 	),
 );
-
+$Cons=0;
+function Col($opc){
+    if($opc==1)
+        $Cons++;
+    else
+        $Cons==0;
+    return $Cons;
+}
 $consulta="SELECT 
 `Fecha`,
 `Seccion`,
@@ -94,132 +101,116 @@ if($cuantos_registros==0){
 }
 else{
 	$indx = 1;
-    $objPHPExcel->getActiveSheet()->setCellValue('A'.$indx, 'Fecha');
-    $objPHPExcel->getActiveSheet()->setCellValue('B'.$indx, 'Seccion');
-    $objPHPExcel->getActiveSheet()->setCellValue('C'.$indx, 'Nip');
-    $objPHPExcel->getActiveSheet()->setCellValue('D'.$indx, 'Numero');
-    $objPHPExcel->getActiveSheet()->setCellValue('E'.$indx, 'CveMunicipio');
-    $objPHPExcel->getActiveSheet()->setCellValue('F'.$indx, 'Res1');
-    $objPHPExcel->getActiveSheet()->setCellValue('G'.$indx, 'Res2');
-    $objPHPExcel->getActiveSheet()->setCellValue('H'.$indx, 'Res3');
-    $objPHPExcel->getActiveSheet()->setCellValue('I'.$indx, 'Res4');
-    $objPHPExcel->getActiveSheet()->setCellValue('J'.$indx, 'Res5');
-    $objPHPExcel->getActiveSheet()->setCellValue('K'.$indx, 'Res6');
-    $objPHPExcel->getActiveSheet()->setCellValue('L'.$indx, 'Res7');
-    $objPHPExcel->getActiveSheet()->setCellValue('M'.$indx, 'Res8');
-    $objPHPExcel->getActiveSheet()->setCellValue('N'.$indx, 'Res9-1-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('O'.$indx, 'Res9-1-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('P'.$indx, 'Res9-1-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('Q'.$indx, 'Res9-1-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('S'.$indx, 'Res9-2-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('T'.$indx, 'Res9-2-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('U'.$indx, 'Res9-2-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('V'.$indx, 'Res9-2-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('X'.$indx, 'Res9-3-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('Y'.$indx, 'Res9-3-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('Z'.$indx, 'Res9-3-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('AA'.$indx, 'Res9-3-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('AC'.$indx, 'Res9-4-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('AD'.$indx, 'Res9-4-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('AE'.$indx, 'Res9-4-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('AF'.$indx, 'Res9-4-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('AH'.$indx, 'Res9-5-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('AI'.$indx, 'Res9-5-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('AJ'.$indx, 'Res9-5-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('AK'.$indx, 'Res9-5-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('AM'.$indx, 'Res9-6-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('AN'.$indx, 'Res9-6-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('AO'.$indx, 'Res9-6-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('AP'.$indx, 'Res9-6-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('AR'.$indx, 'Res9-7-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('AS'.$indx, 'Res9-7-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('AT'.$indx, 'Res9-7-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('AU'.$indx, 'Res9-7-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('AW'.$indx, 'Res9-8-1');
-    $objPHPExcel->getActiveSheet()->setCellValue('AX'.$indx, 'Res9-8-2');
-    $objPHPExcel->getActiveSheet()->setCellValue('AY'.$indx, 'Res9-8-3');
-    $objPHPExcel->getActiveSheet()->setCellValue('AZ'.$indx, 'Res9-8-4');
-    
-    $objPHPExcel->getActiveSheet()->setCellValue('BQ'.$indx, 'Res10');
-    $objPHPExcel->getActiveSheet()->setCellValue('BR'.$indx, 'Res11');
-    $objPHPExcel->getActiveSheet()->setCellValue('BS'.$indx, 'Res12');
-    $objPHPExcel->getActiveSheet()->setCellValue('BT'.$indx, 'Res13');
-    $objPHPExcel->getActiveSheet()->setCellValue('BU'.$indx, 'Res14');
-    $objPHPExcel->getActiveSheet()->setCellValue('BV'.$indx, 'Res15');
-    $objPHPExcel->getActiveSheet()->setCellValue('BW'.$indx, 'Res16');
-    $objPHPExcel->getActiveSheet()->setCellValue('BX'.$indx, 'Res17');
-    $objPHPExcel->getActiveSheet()->setCellValue('BY'.$indx, 'Res18');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Fecha');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Seccion');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Nip');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Numero');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'CveMunicipio');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res5');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res6');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res7');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res8');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-1-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-1-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-1-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-1-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-2-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-2-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-2-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-2-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-3-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-3-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-3-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-3-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-4-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-4-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-4-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-4-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-5-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-5-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-5-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-5-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-6-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-6-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-6-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-6-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-7-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-7-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-7-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-7-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-8-1');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-8-2');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-8-3');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res9-8-4');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res10');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res11');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res12');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res13');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res14');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res15');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res16');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res17');
+    $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, 'Res18');
     $indx = 2;
 	while($MostrarFila=$bd->fetch_array($Resultado)){
-		$objPHPExcel->getActiveSheet()->setCellValue('A'.$indx, $MostrarFila['Fecha']);
-		$objPHPExcel->getActiveSheet()->setCellValue('B'.$indx, $MostrarFila['Seccion']);
-		$objPHPExcel->getActiveSheet()->setCellValue('C'.$indx, $MostrarFila['Nip']);
-		$objPHPExcel->getActiveSheet()->setCellValue('D'.$indx, $MostrarFila['Numero']);
-		$objPHPExcel->getActiveSheet()->setCellValue('E'.$indx, $MostrarFila['CveMunicipio']);
-		$objPHPExcel->getActiveSheet()->setCellValue('F'.$indx, $MostrarFila['Res1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('G'.$indx, $MostrarFila['Res2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('H'.$indx, $MostrarFila['Res3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('I'.$indx, $MostrarFila['Res4']);
-		$objPHPExcel->getActiveSheet()->setCellValue('J'.$indx, $MostrarFila['Res5']);
-		$objPHPExcel->getActiveSheet()->setCellValue('K'.$indx, $MostrarFila['Res6']);
-		$objPHPExcel->getActiveSheet()->setCellValue('L'.$indx, $MostrarFila['Res7']);
-		$objPHPExcel->getActiveSheet()->setCellValue('M'.$indx, $MostrarFila['Res8']);
-		$objPHPExcel->getActiveSheet()->setCellValue('N'.$indx, $MostrarFila['Res9-1-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('O'.$indx, $MostrarFila['Res9-1-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('P'.$indx, $MostrarFila['Res9-1-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('Q'.$indx, $MostrarFila['Res9-1-4']);
-        
-        $objPHPExcel->getActiveSheet()->setCellValue('S'.$indx, $MostrarFila['Res9-2-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('T'.$indx, $MostrarFila['Res9-2-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('U'.$indx, $MostrarFila['Res9-2-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('V'.$indx, $MostrarFila['Res9-2-4']);
-        
-        $objPHPExcel->getActiveSheet()->setCellValue('X'.$indx, $MostrarFila['Res9-3-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('Y'.$indx, $MostrarFila['Res9-3-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('Z'.$indx, $MostrarFila['Res9-3-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AA'.$indx, $MostrarFila['Res9-3-4']);
-        
-        $objPHPExcel->getActiveSheet()->setCellValue('AC'.$indx, $MostrarFila['Res9-4-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AD'.$indx, $MostrarFila['Res9-4-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AE'.$indx, $MostrarFila['Res9-4-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AF'.$indx, $MostrarFila['Res9-4-4']);
-        
-        $objPHPExcel->getActiveSheet()->setCellValue('AH'.$indx, $MostrarFila['Res9-5-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AI'.$indx, $MostrarFila['Res9-5-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AJ'.$indx, $MostrarFila['Res9-5-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AK'.$indx, $MostrarFila['Res9-5-4']);
-        
-        $objPHPExcel->getActiveSheet()->setCellValue('AM'.$indx, $MostrarFila['Res9-6-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AN'.$indx, $MostrarFila['Res9-6-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AO'.$indx, $MostrarFila['Res9-6-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AP'.$indx, $MostrarFila['Res9-6-4']);
-        
-        $objPHPExcel->getActiveSheet()->setCellValue('AR'.$indx, $MostrarFila['Res9-7-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AS'.$indx, $MostrarFila['Res9-7-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AT'.$indx, $MostrarFila['Res9-7-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AU'.$indx, $MostrarFila['Res9-7-4']);
-        
-        $objPHPExcel->getActiveSheet()->setCellValue('AW'.$indx, $MostrarFila['Res9-8-1']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AX'.$indx, $MostrarFila['Res9-8-2']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AY'.$indx, $MostrarFila['Res9-8-3']);
-		$objPHPExcel->getActiveSheet()->setCellValue('AZ'.$indx, $MostrarFila['Res9-8-4']);
-        
-		$objPHPExcel->getActiveSheet()->setCellValue('BQ'.$indx, $MostrarFila['Res10']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BR'.$indx, $MostrarFila['Res11']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BS'.$indx, $MostrarFila['Res12']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BT'.$indx, $MostrarFila['Res13']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BU'.$indx, $MostrarFila['Res14']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BV'.$indx, $MostrarFila['Res15']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BW'.$indx, $MostrarFila['Res16']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BX'.$indx, $MostrarFila['Res17']);
-		$objPHPExcel->getActiveSheet()->setCellValue('BY'.$indx, $MostrarFila['Res18']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Fecha']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Seccion']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Nip']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Numero']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['CveMunicipio']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res4']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res5']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res6']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res7']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res8']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-1-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-1-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-1-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-1-4']);
+        $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-2-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-2-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-2-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-2-4']);
+        $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-3-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-3-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-3-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-3-4']);
+        $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-4-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-4-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-4-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-4-4']);
+        $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-5-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-5-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-5-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-5-4']);
+        $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-6-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-6-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-6-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-6-4']);
+        $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-7-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-7-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-7-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-7-4']);
+        $objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-8-1']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-8-2']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-8-3']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res9-8-4']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res10']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res11']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res12']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res13']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res14']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res15']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res16']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res17']);
+		$objPHPExcel->getActiveSheet()->setCellValue(Col().$indx, $MostrarFila['Res18']);
 		$indx++; 
 	}
 	$nomb .='.xlsx';
