b6a182b70c7523dea51579185ac1e4e07c20c946
Arturo Corona <arturo44full@gmail.com>
2018-04-15 15:43:52 -0500

    21

--- CapturaLaFragua/Reportes/Respuestas.php
+++ CapturaLaFragua/Reportes/Respuestas.php
@@ -18,7 +18,7 @@ endif;
 header('Content-Type: text/html; charset=ISO-8859-1');
 
 $fecha1 = date('Y-m-d');
-$nomb = 'Concentrado frecuencias '.$fecha1;
+$nomb = 'Concentrado Medicion '.$fecha1;
 
 $styleArray = array(
 	'borders' => array(
@@ -29,7 +29,7 @@ $styleArray = array(
 	),
 );
 $IdxAbc=0;
-$Abc = array("B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+$Abc = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
               "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ",
               "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ",
               "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ", "CK", "CL", "CM", "CN", "CO", "CP", "CQ", "CR", "CS", "CT", "CU", "CV", "CW", "CX", "CY", "CZ",);
@@ -40,11 +40,12 @@ if($cuantos_registros==0){
 	exit;
 }
 else{
+	
 	$ini = 0;
 	while($MostrarFila=$bd->fetch_array($Resultado)){
         $indx = 4;
         if($ini>8){
-            $sql="SELECT `".$MostrarFila['COLUMN_NAME']."` Res, count(*)Total FROM captura_lafragua
+            $sql="SELECT `".$MostrarFila['COLUMN_NAME']."` Res, count(*)Total FROM captura_lafragua 
                 where `".$MostrarFila['COLUMN_NAME']."` <> 0 and `".$MostrarFila['COLUMN_NAME']."` is not null
                 group by `".$MostrarFila['COLUMN_NAME']."`"; 
             if($Res=$bd->consulta($sql)){
@@ -58,20 +59,17 @@ else{
                             //$objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx, $MostrarFila['COLUMN_NAME']);
                             //$objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx, $mivalor['Res']);
                             while($indice!=$mivalor['Res']){
-                                if($mivalor['Res']){
-                                    
-                                }
-                                $objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx,'0');
+                                $objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx,($indice.") 0"));
                                 $indice++;
                                 $indx++;
                             }
-                            $objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx,($mivalor['Total']));
+                            $objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx,($mivalor['Res'].") ".$mivalor['Total']));
                             $indx++;
                             $indice++;
                         }
                     }
                     else{
-                       // echo "<br>1".$sql;
+                        echo "<br>1".$sql;
                     }
                 }
                 else{
@@ -85,6 +83,7 @@ else{
         }
         $ini++;
     }
+    
 	$nomb .='.xlsx';
 	header('Content-Type: application/vnd.ms-excel');
 	header('Content-Disposition: attachment;filename="'.$nomb.'"');
