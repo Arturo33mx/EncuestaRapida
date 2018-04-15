c00cd91cb24322ce6795d88c98ce1a3ccfd5d288
Arturo Corona <arturo44full@gmail.com>
2018-04-15 15:25:03 -0500

    a

--- CapturaLaFragua/Reportes/Respuestas.php
+++ CapturaLaFragua/Reportes/Respuestas.php
@@ -18,12 +18,7 @@ endif;
 header('Content-Type: text/html; charset=ISO-8859-1');
 
 $fecha1 = date('Y-m-d');
-$nomb = 'Concentrado Medicion '.$fecha1;
-
-if(isset($_GET['Muni']))
-    $Muni=$_GET['Muni'];
-else
-    exit;
+$nomb = 'Concentrado frecuencias '.$fecha1;
 
 $styleArray = array(
 	'borders' => array(
@@ -38,7 +33,7 @@ $Abc = array("B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O
               "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ",
               "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ",
               "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ", "CK", "CL", "CM", "CN", "CO", "CP", "CQ", "CR", "CS", "CT", "CU", "CV", "CW", "CX", "CY", "CZ",);
-$consulta="select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'captura_dis22_nueva2';"; 
+$consulta="select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'captura_lafragua';"; 
 $Resultado=$bd->consulta($consulta);
 $cuantos_registros= $bd->num_rows($Resultado);
 if($cuantos_registros==0){
@@ -49,8 +44,8 @@ else{
 	while($MostrarFila=$bd->fetch_array($Resultado)){
         $indx = 4;
         if($ini>8){
-            $sql="SELECT `".$MostrarFila['COLUMN_NAME']."` Res, count(*)Total FROM captura_dis22_nueva2
-                where `".$MostrarFila['COLUMN_NAME']."` <> 0 and `".$MostrarFila['COLUMN_NAME']."` is not null and CveMunicipio=$Muni
+            $sql="SELECT `".$MostrarFila['COLUMN_NAME']."` Res, count(*)Total FROM captura_lafragua
+                where `".$MostrarFila['COLUMN_NAME']."` <> 0 and `".$MostrarFila['COLUMN_NAME']."` is not null
                 group by `".$MostrarFila['COLUMN_NAME']."`"; 
             if($Res=$bd->consulta($sql)){
                 $total= $bd->num_rows($Res);
@@ -63,6 +58,9 @@ else{
                             //$objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx, $MostrarFila['COLUMN_NAME']);
                             //$objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx, $mivalor['Res']);
                             while($indice!=$mivalor['Res']){
+                                if($mivalor['Res']){
+                                    
+                                }
                                 $objPHPExcel->getActiveSheet()->setCellValue($Abc[$IdxAbc].$indx,'0');
                                 $indice++;
                                 $indx++;
