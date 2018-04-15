2306b7b312748e8761d86b5b477de721d94fac8f
Arturo Corona <arturo44full@gmail.com>
2018-04-15 15:44:41 -0500

    y

--- CapturaLaFragua/Reportes/Respuestas.php
+++ CapturaLaFragua/Reportes/Respuestas.php
@@ -33,7 +33,7 @@ $Abc = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
               "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ",
               "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ",
               "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ", "CK", "CL", "CM", "CN", "CO", "CP", "CQ", "CR", "CS", "CT", "CU", "CV", "CW", "CX", "CY", "CZ",);
-$consulta="select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'captura_lafragua';"; 
+$consulta="select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'captura_distrito22';"; 
 $Resultado=$bd->consulta($consulta);
 $cuantos_registros= $bd->num_rows($Resultado);
 if($cuantos_registros==0){
@@ -45,7 +45,7 @@ else{
 	while($MostrarFila=$bd->fetch_array($Resultado)){
         $indx = 4;
         if($ini>8){
-            $sql="SELECT `".$MostrarFila['COLUMN_NAME']."` Res, count(*)Total FROM captura_lafragua 
+            $sql="SELECT `".$MostrarFila['COLUMN_NAME']."` Res, count(*)Total FROM captura_distrito22 
                 where `".$MostrarFila['COLUMN_NAME']."` <> 0 and `".$MostrarFila['COLUMN_NAME']."` is not null
                 group by `".$MostrarFila['COLUMN_NAME']."`"; 
             if($Res=$bd->consulta($sql)){
